0defa7ae5d24bc76c6b9fb2fbfdefcd12f6a4a95
Yury Kundin <yury.kundin@yandex.ru>
2023-10-01 18:13:47 +0300

    Add the Vite

--- frontend/infra/entities/http-server.ts
+++
@@ -1,3 +0,0 @@
-export interface HttpServer {
-  listen(port: number, callback?: () => void): void;
-}
--- frontend/run-dev-mode.ts
+++
@@ -1,8 +0,0 @@
-import { BunHttpServer } from "~/infra/bun-http-server";
-
-const port = Number(process.env.PORT) || 3001;
-const server = new BunHttpServer();
-
-server.listen(port, () => {
-  console.log(`Application is running on port ${port}!`);
-});
