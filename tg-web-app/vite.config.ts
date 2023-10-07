753f13af4d79379312ec33d0fdf8362adafbb2d6
Yury Kundin <yury.kundin@yandex.ru>
2023-10-08 01:01:11 +0300

    Add the info about docker-compose

--- tg-web-app/vite.config.ts
+++ tg-web-app/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: Number(process.env.PORT) || 3000,
+  },
   plugins: [react()],
-})
+});
