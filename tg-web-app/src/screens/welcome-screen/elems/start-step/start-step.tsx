dd8f3622bb9b4de8f52eb69f1aba5bb009fa57e9
Yury Kundin <yury.kundin@yandex.ru>
2023-10-07 22:45:11 +0300

    Add info about expand

--- tg-web-app/src/screens/welcome-screen/elems/start-step/start-step.tsx
+++ tg-web-app/src/screens/welcome-screen/elems/start-step/start-step.tsx
@@ -1,6 +1,9 @@
+import { useEffect } from "react";
+
+import logotype from "./images/logotype.png";
 import styles from "./start-step.module.css";
 
-import { type HTMLAttributes, type FC, useEffect } from "react";
+import type { HTMLAttributes, FC } from "react";
 
 export interface StartStepProps extends HTMLAttributes<HTMLDivElement> {
   className?: string;
@@ -13,14 +16,17 @@ const StartStep: FC<StartStepProps> = (props) => {
   useEffect(() => {
     const id = setTimeout(() => {
       if (onEnd) onEnd();
-    }, 2000);
+    }, 3000);
 
     return () => clearTimeout(id);
   }, [onEnd]);
 
   return (
     <div className={styles.step}>
-      <div className={styles.logo} />
+      <div className={styles.logo}>
+        <img className={styles.image} src={logotype} alt="" />
+      </div>
+
       <div className={styles.text}>
         I will help you
         <br />
