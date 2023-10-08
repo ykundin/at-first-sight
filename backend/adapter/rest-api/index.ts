a2b116a62104ff4e1b061e44e504f3a9ca1613c1
Yury Kundin <yury.kundin@yandex.ru>
2023-10-08 23:52:12 +0300

    Move the recommendations on backend

--- backend/adapter/rest-api/index.ts
+++ backend/adapter/rest-api/index.ts
@@ -2,10 +2,12 @@ import { commonRoutes } from "./common";
 import { authRoutes } from "./auth";
 import { paymentsRoutes } from "./payments";
 import { fireRoutes } from "./fire";
+import { recommendationsRoutes } from "./recommendations";
 
 export const restApiRoutes = [
   ...commonRoutes,
   ...authRoutes,
   ...paymentsRoutes,
   ...fireRoutes,
+  ...recommendationsRoutes,
 ];
