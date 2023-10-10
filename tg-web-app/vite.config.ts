7895c79c047dddc769e2247baf9157c4ca3fd3d8
Yury Kundin <yury.kundin@yandex.ru>
2023-10-10 05:13:03 +0300

    Remove proxy from tg-web-app

--- tg-web-app/vite.config.ts
+++ tg-web-app/vite.config.ts
@@ -5,17 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     port: Number(process.env.PORT) || 3000,
-    proxy: {
-      "/image": {
-        target: process.env.IMGPROXY_URL,
-        rewrite(path) {
-          const key = path.split("/")[2];
-          const bucket = process.env.AWS_BUCKET;
-
-          return `/insecure/plain/s3://${bucket}/${key}`;
-        },
-      },
-    },
   },
   plugins: [react()],
 });
